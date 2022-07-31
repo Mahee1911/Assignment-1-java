9530cbf983d0bbaebb697ad4759864b7d21a8528
Mahee1911 <110080237+Mahee1911@users.noreply.github.com>
2022-07-31 18:18:26 +0530

    Add files via upload

---
+++ pr3.java
@@ -0,0 +1,34 @@
+//21CE029 : MAHEE GADHIYA 
+package com.info;
+import java.util.*;
+public class pr3 {
+	//logic of check same last digit in boolean form
+    static boolean lastDigit(int a,int b)
+    {
+    	if(a%10 == 0 && b% 10 == 0)
+        {
+            return true;
+        }
+        else if(b % 10 ==a || a % 10 == b || a ==b ){
+            return true;
+        }
+        else 
+        return false ; }
+    //main method
+    public static void main(String[] args) {
+    	//object of scanner class
+        Scanner sc = new Scanner(System.in);
+        //enter first number
+        System.out.println("Enter 1st no");
+        //scanning value of number 1
+        int a = sc.nextInt();
+        //enter second number
+        System.out.println("Enter 2nd no");
+        //scanning value of number 2
+        int b = sc.nextInt();
+        //call function lastdigit();
+        System.out.println(lastDigit(a,b));
+    }
+}
+
+
